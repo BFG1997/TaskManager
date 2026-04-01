d2dfedd2ac7581fb822a4e0d95cc4b524d878da0
BFG1997 <babkinvvns@mail.ru>
2026-04-01 18:10:37 +0500

    Create ErrorService.cs

---
+++ TaskManager/TaskManager.App/Services/Interfaces/ErrorService.cs
@@ -0,0 +1,43 @@
+﻿
+
+namespace TaskManager.App.Services.Interfaces
+{
+    public class ErrorService : IErrorService
+    {
+        public async Task<(bool isSuccess, string? message)> IsRequestSuccessful(HttpResponseMessage? response)
+        {
+            var message = string.Empty;
+            if (response == null)
+            {
+                message = "Сервер недоступен";
+                Console.Error.WriteLine("Сервер недоступен");
+                return (false, message);
+            }
+
+            if (response.IsSuccessStatusCode)
+            {
+                return (true, string.Empty);
+
+            }
+            else if (response.StatusCode == System.Net.HttpStatusCode.BadRequest)
+            {
+                var errors = await response.Content.ReadFromJsonAsync<Dictionary<string, string[]>>();
+
+                if (errors != null)
+                {
+                    message = string.Join("\n", errors.SelectMany(x => x.Value));
+                }
+                else
+                {
+                    message = "Validation error";
+                }
+                return (false, message);
+            }
+            else
+            {
+                message = $"Ошибка: {response.StatusCode}";
+                return (false, message);
+            }
+        }
+    }
+}
